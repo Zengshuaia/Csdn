d6215b608e460682c730c58ab09cfba0af2212af
zengshuai <2741903696@qq.com>
2023-06-07 18:52:44 +0800

    安装完Layui

---
+++ Csdn/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Csdn.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Csdn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
